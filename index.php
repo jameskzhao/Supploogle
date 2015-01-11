4b417145bf80ea2a0072ef763d2c4e7636940094
jameskzhao <james.k.zhao@gmail.com>
2015-01-10 16:02:52 -0800

    Bootstrap styling

--- index.php
+++ index.php
@@ -1,140 +1,63 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-
-<!doctype html>
-
-
-<html>
-
-<head>
-
-
-    <!-- META -->
-    <meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="" width="device-width">
-    <meta name="google" value="notranslate" />
-
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Supploogle</title>
+        <!-- Goole font for logo -->
+        <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     
-    <!-- TITLE -->
-    <title>
-	Supploogle: suppliers, China
-    </title>
-</head>
+        <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+    <!-- Optional theme 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+-->
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/btstyle.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 <body>
-    <div id="main_wrapper">
-        <div id="home_logo">
-            <a href="index.php">Supploogle</a>
-        </div>
-        <nav id="top_nav_menu">
-            <a href="" class="active">Home</a>
-            <a href="#">About Us</a>
-            <a href="#">Contact Us</a>
-            <a href="#">Login</a>
-        </nav>
-        <div id="home_search_wrapper">
+    <nav class="navbar navbar-default bg-navbar" role="navigation">
+        <div class="container-fluid">
+            <div class="row">
+                <div style=" text-aligh:center; padding-left: 2px; padding-right: 2px; font-size:36px;" class="col-sm-2">
+                    <div class="navbar-header full-width">
+                        <a class="block-elem full-width vertical-aligned text-left no-underline" style="height: 44px" href="/cn/">
+                            Supploogle</a>
+                    </div>
+                </div>
+                <div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li> 
+                        <li><a href="#">Page 3</a></li> 
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
+            </div>
             
-            <div id="home_search">
-            <h1>Find your supplier. Quick and easy.</h1>
-                <form class="form_wrapper">
-                    <div id="search_bar">
-                    <input type="text" placeholder="Try a keyword in company or city name..." required>
-                    </div>
-                    <div id="selector_bar">
-                        <div class="styled-select slate">
-                            <select id="category_selector">
-                                <option value="">Choose Category</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                            </select>
-                        </div>
-                        <div class="styled-select slate">
-                            <select id="sub_category_selector">
-                                <option value="">Choose Sub-Category</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                            </select>
-                        </div>
-                        <div class="styled-select slate">
-                            <select id="sub_sub_category_selector">
-                                <option value="">Choose Sub-sub-Category</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                                <option value="">Test Category 1</option>
-                            </select>
-                        </div>
-                        <div class="styled-select slate">    
-                            <select id="country_selector">
-                                <option value="">Choose Country</option>
-                                <option value="cn">China</option>
-                                <option value="ca">Canada</option>
-                                <option value="us">United States</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div id="checkbox_bar">
-                        <div class="single_checkbox">
-                            <input type="checkbox" id="ts" class="home_checkbox">
-                            <label for="ts">TS</label>
-                        </div>
-                        <div class="single_checkbox">
-                            <input type="checkbox" id="iso" class="home_checkbox">
-                            <label for="iso">ISO</label>
-                        </div>
-                        <div class="single_checkbox">
-                            <input type="checkbox" id="as" class="home_checkbox">
-                            <label for="as">AS</label>
-                        </div>
-                        <div class="single_checkbox">
-                            <input type="checkbox" id="duns" class="home_checkbox">
-                            <label for="duns">DUNS</label>
-                        </div>
-                    </div>
-                    
-                    <div id="right_corner_button">
-                        <button class="disabled" type="submit">Search</button>
-                    </div>
-                    
-                </form>
-            </div>
         </div>
-        <div id="home_map_wrapper">
-            <img src="images/background_map.png"/>
-        </div>                 
-        <div id="home_footer">
-            <div id="supploogle_intro">
-                One sentence introduction about Supploogle. <a href="">More info...</a>
-            </div>
-            <div id="social_buttons">
-                <a href=""><div class="social_icon facebook_button" ></div></a>
-                <a href=""><div class="social_icon twitter_button" ></div></a>
-                <a href=""><div class="social_icon linkedin_button" ></div></a>
-            </div>
-        </div>
+    </nav>
+    <div class="container-fluid"  role="main">
+        <div class="row upper"></div>
     </div>
-    
-</body>
+
+  </body>
 </html>
